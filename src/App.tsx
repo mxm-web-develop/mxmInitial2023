5497ae7fb14d640ff0efa5c4f790022507efe62e
MxM <zhanghanfeng@mxm-mini.local>
2023-03-12 20:11:06 +0800

    merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
 import Label from './components/Label'
-
+import { motion } from "framer-motion"
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="flex px-40 items-center justify-center text-center">
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5,x:-200 }}
+          animate={{ opacity: 1, scale: 1.5,x:0 }}
+          transition={{ duration: 1.2 }}
+        >
       <Label />
+      </motion.div>
     </div>
   )
 }
