3e8a8fa61d652e03f7d1ee483aa538082a16e5f8
MxM <zhanghanfeng@mxm-mini.local>
2023-03-12 14:31:13 +0800

    version auto

---
+++ scripts/devVersion.cjs
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const packageJson = fs.readFileSync('../package.json');
+const packageObj = JSON.parse(packageJson);
+const version = JSON.parse(fs.readFileSync('../package.json')).version
+const { exec,execSync } = require('child_process');
+// const commitMsg = execSync('git log --format=%B -n 1 HEAD').toString().trim();
+const branchName = execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
+
+let versionUpdate;
+exec('git show -s ', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    const [commit] = stdout.trim().split('\n');
+    versionUpdate = version+`_${branchName+commit.substr(-5)}`
+    console.log(versionUpdate);
+    packageObj.version = versionUpdate;
+    const newPackageJson = JSON.stringify(packageObj, null, 2);
+    fs.writeFileSync('../package.json', newPackageJson);
+
+});
