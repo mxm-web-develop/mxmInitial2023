9c2ff4e3701ed057efdca169bb4a56f4d086f26f
MxM <zhanghanfeng@mxm-mini.local>
2023-03-12 16:16:14 +0800

    devhusky

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import Label from './components/Label'
+import { motion } from "framer-motion"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="flex px-40 items-center justify-center text-center">
+      
       <Label />
     </div>
   )
